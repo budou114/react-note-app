4acbb577b99b35b35a9628661920038efeab2705
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-18 08:51:34 +0900

    ノートを選択したらハイライトされる仕組みを作ろう

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import uuid from "react-uuid";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState(false);
 
   const onAddNote = () => {
     const newNote = {
@@ -28,6 +29,8 @@ function App() {
         onAddNote={onAddNote}
         notes={notes}
         onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
       />
       <Main />
     </div>
